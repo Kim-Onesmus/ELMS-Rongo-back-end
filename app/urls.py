ab77d49c750d14a08cf61781384c4380992a89b9
unknown <kimones1122@gmail.com>
2023-06-06 14:37:01 +0300

    updates

--- app/urls.py
+++ app/urls.py
@@ -50,10 +50,21 @@ urlpatterns = [
     path('delete_job/<str:pk>/', views.deleteJob, name='delete_job'),
     path('search', views.Search, name='search'),
     
-    # <===========Password Reset ===========>
-    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="app/passwordReset/forget-password.html"), name="reset_password"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="app/passwordReset/password_reset_sent.html"), name="password_reset_done"),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="app/passwordReset/password_reset_form.html"), name="password_reset_confirm"),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="app/passwordReset/password_reset_done.html"), name="password_reset_complete")
+    # <===========Password Reset ===========>    
+    path('reset_password/',
+     auth_views.PasswordResetView.as_view(template_name="app/passwordReset/forget-password.html"),
+     name="reset_password"),
+
+    path('reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(template_name="app/passwordReset/password_reset_sent.html"), 
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(template_name="app/passwordReset/password_reset_form.html"), 
+     name="password_reset_confirm"),
+
+    path('reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name="app/passwordReset/password_reset_done.html"), 
+        name="password_reset_complete"),
 
 ]
